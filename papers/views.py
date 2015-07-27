3fd2ce0bd42de9a7c8ebf43bdef641dac8dee09e
Sven Heinicke <sven@heinicke.org>
2015-07-27 15:00:02 -0400

    it now does an and search

--- papers/views.py
+++ papers/views.py
@@ -25,114 +25,74 @@ class PaperIndexView(generic.ListView):
     template_name = 'papers/index.html'
     context_object_name = 'papers_list'
 
-    the_filter = Q() # this should be overloaded
-    queryset = Paper.objects.filter(the_filter).distinct().order_by('pub_date')
-
+    the_filter = False # this should be overloaded
     GOT = False # for caching multiple requests
     template_ref = None # for tweaking with the template
 
-    @classmethod
-    def filtered_count(cls,got):
-        """Filter queryset through the_filter of this object and return how many we have."""
-        return len(cls.filtered_list(got))
-
-    @classmethod
-    def filtered_list(cls,got,qs=False):
-        """Returns a list of papers that match the query.  We assume got
-        argument is already scrubbed."""
-
-        out=None
-        if qs:
-            # Sometimes we already have the first bit of the query, we
-            # don't need to redo it.
-            out=set(list(qs.filter(cls.get_filter(got)).distinct()))
-        else:
-            # sometimes we don't
-            out=set(list(Paper.objects.filter(cls.get_filter(got)).distinct()))
-
-        # we have already filtered by PMID, so lets get rid of the numbers.
-        words=[]
-        for item in got.getlist('s'):
-            if not(item.isdigit()):
-                words.append(item)
-
-        # If there is nothing else to search for we can bail now.
-        if 0 == len(words):
-            return out
-
-        # logical or all the words together
-        q=[]
-        for word in words:
-            q.append(Q(name__contains=word))
-        # we want to search the name column in both Observable2 and
-        # ConditionsType
-
-        # now we get phenotypes
-        os=Observable2.objects.filter(reduce(operator.or_,q))
-        for o in os:
-            # filter out papers not of out type
-            out.update(o.paper_list().filter(cls.the_filter))
-
-        # in the conditionType search we also want to check the
-        # "short_name" column.
-        for word in words:
-            q.append(Q(short_name__contains=word))
-
-        # here we get conditions
-        cs=ConditionType.objects.filter(reduce(operator.or_,q))
-        for c in cs:
-            out.update(c.paper_list().filter(cls.the_filter))
-
-        return out
-
-    @classmethod
-    def get_filter(cls,got):
-        """Returns a filter for the query specified in the, already scrubbed,
-        got variable."""
-
-        q=[]
-
-        if 's' in got:
-            # we assume already scrubbed
-            esses=got.getlist('s')
-            for s in esses:
-                if s.isdigit():
-                    q.append(Q(pmid__contains=s))
-                else:
-                    q.extend([Q(first_author__contains=s),Q(last_author__contains=s)])
-
-        # Now we want to OR everything in q, then and it with
-        # qs.the_filter, but we don't want to do more then we need.
-
-        if 1 == len(q):
-            return reduce(operator.and_,[cls.the_filter,q[0]])
-        elif 1 < len(q):
-            return reduce(operator.and_,[cls.the_filter,reduce(operator.or_,q)])
-        return cls.the_filter # if 0==len(q)
-
     def get_queryset(self):
-        """Returns the Paper.objects filtered with get_filter and self.request.GET."""
-        return Paper.objects.filter(self.get_filter(self._scrub_GET())).distinct()
+
+        got=self.scrub_GET()
+        if 's' in got:
+            papers=[] # hold list of paper ids
+
+            for s in got.getlist('s'):
+                o2s=Observable2.objects.filter(name__contains=s)
+                cts=ConditionType.objects.filter(
+                    Q(name__contains=s)|Q(short_name__contains=s))
+
+                ps=set()
+                for o2 in o2s:
+                    for paper in o2.paper_list():
+                        ps.add(paper.id)
+                for ct in cts:
+                    for paper in ct.paper_list():
+                        ps.add(paper.id)
+
+                f=None
+                if s.isdigit():
+                    # we don't need to check PMID unless it's all digits
+                    f=(Q(first_author__contains=s)|Q(last_author__contains=s)|
+                       Q(pmid__contains=s))
+                else:
+                    f=Q(first_author__contains=s)|Q(last_author__contains=s)
+
+                for paper in Paper.objects.exclude(pk__in=ps).filter(f):
+                    ps.add(paper.id)
+
+                papers.append(ps)
+
+            if 0 == len(papers):
+                return []
+            else:
+                # We want to return a ResultSet because we need to
+                # filter it again later.
+                return Paper.objects.filter(pk__in=set.intersection(*papers))
+
+        # if no query return everything
+        return super(PaperIndexView, self).get_queryset()
+
 
     @classmethod
     def scrub_GET_txt(self,get):
         """Returns text suitable for putting in the search box."""
-        return self.got_txt(self.scrub_GET(get))
+        return self.got_txt(self._scrub_GET(get))
 
     @classmethod
     def got_txt(self,got):
         """Same as scrub_GET_txt() but assume got is already scrubbed."""
         return ' '.join(got.getlist('s'))
 
-    def _scrub_GET(self):
-        """Returns a scrubbed self.request.GET"""
+    def scrub_GET(self):
+        """Returns a scrubbed self.request.GET, scrub it if we haven't done so
+        yet."""
         if not(self.GOT):
-            self.GOT=self.scrub_GET(self.request.GET)
+            self.GOT=self._scrub_GET(self.request.GET)
         return self.GOT
 
     @classmethod
-    def scrub_GET(self,GET):
-        "Scrubbs a QueryDict object."
+    def _scrub_GET(self,GET):
+        """Scrubbs a QueryDict object.  This function is subject to change
+        without notice."""
         OUT=QueryDict(mutable=True)
 
         # their can only be one 'debug' items and it must equal an
@@ -149,7 +109,8 @@ class PaperIndexView(generic.ListView):
             s=[]
             for ess in esses:
                 s.extend(ess.strip().split())
-            OUT.setlist('s',s)
+            if 0 < len(s):
+                OUT.setlist('s',s)
 
         # anything else is hacking.
         return OUT
@@ -160,27 +121,32 @@ class PaperIndexView(generic.ListView):
         key='num_%s' % (cls.template_ref)
 
         if type(self_) == cls:
-            out[key]=len(qs)
-            #out[key]=cls.filtered_count(got)
+            if cls.the_filter:
+                qs=qs.filter(cls.the_filter).distinct().\
+                    order_by('pub_date','pmid')
+                out['papers_list']=qs
+            out[key]=len(qs) # qs.count()
             out['sub_navigation']=cls.template_ref.replace('_',' ').capitalize()
         else:
-            out[key]=cls.filtered_count(got)
+            if cls.the_filter:
+                out[key]=qs.filter(cls.the_filter).distinct().count()
+            else:
+                out[key]=len(qs)
         return out
 
     def get_context_data(self, **kwargs):
         """Message the default context data to something we can use."""
 
         context = super(PaperIndexView, self).get_context_data(**kwargs)
-        got=self._scrub_GET()
+        got=self.scrub_GET()
 
         # filter out by pmid and author
         papers=context['papers_list']
-
-        papers=self.filtered_list(got,qs=papers)
-        # at this pount papers is a list, no a QuerySet.
+        # Every thing is papers is in the results, now we need to
+        # check everything else.  Bleck.
 
         context['s']=self.got_txt(got)
-        context['papers_list']=papers
+        print context['s']
         context['verbose']=got.get('verbose')
         context['got'] = '?%s' % (got.urlencode())
         if '?' == context['got']:
