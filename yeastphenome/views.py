6d3a2da6426dbfa6af33868fc5c4f1690e472683
Sven Heinicke <sven@heinicke.org>
2015-09-16 13:51:06 -0400

    home page loads a wee bit faster

--- yeastphenome/views.py
+++ yeastphenome/views.py
@@ -7,31 +7,32 @@ from papers.views import PaperIndexView
 import datetime
 
 def index(request):
-    paper_num_total = Paper.objects.all().count()
-    paper_num_with_data = Data.objects.values('dataset__paper').distinct().count()
-    phenotype_num_total = Observable2.objects.exclude(phenotype__dataset = None).count()
-    phenotype_num_with_data = Data.objects.values('dataset__phenotype__observable2__name').distinct().count()
-    condition_num = Condition.objects.all().count()
     latest_paper = Paper.objects.all().latest().modified_on
     latest_condition = Condition.objects.all().latest().modified_on
-
-    most_studied_phenotype = Observable2.objects.annotate(num_datapoints=Sum('phenotype__dataset__tested_num')).exclude(num_datapoints=None).order_by('-num_datapoints').first()
-    most_studied_conditionset = ConditionSet.objects.exclude(name='standard').exclude(name='').values('name').annotate(num_datapoints=Sum('dataset__tested_num')).order_by('-num_datapoints').first()
 
     if latest_condition is None:
         # fudging NULL entries in the database
         latest_condition = datetime.date(1,1,1)
 
-    context = {'paper_num_total': paper_num_total, 'paper_num_with_data': paper_num_with_data,
-               'phenotype_num_total': phenotype_num_total, 'phenotype_num_with_data': phenotype_num_with_data,
-               'condition_num': condition_num,
-               'most_studied_conditionset': most_studied_conditionset,
-               'most_studied_phenotype': most_studied_phenotype,
-               'updated': max(latest_paper, latest_condition),
+    context = {
+        'paper_num_total': Paper.objects.all().count(),
+        'phenotype_num_total': Observable2.objects.exclude(phenotype__dataset = None).count(),
+        'condition_num': Condition.objects.all().count(),
+        'updated': max(latest_paper, latest_condition),
 
-               # To repopulate the search box
-               's':PaperIndexView.scrub_GET_txt(request.GET)
-               }
+        # To repopulate the search box
+        's':PaperIndexView.scrub_GET_txt(request.GET),
+    }
+
+    if 'verbose' in request.GET:
+        context.update(
+            {
+                'paper_num_with_data': Data.objects.values('dataset__paper').distinct().count(),
+                'phenotype_num_with_data': Data.objects.values('dataset__phenotype__observable2__name').distinct().count(),
+                'most_studied_conditionset': ConditionSet.objects.exclude(name='standard').exclude(name='').values('name').annotate(num_datapoints=Sum('dataset__tested_num')).order_by('-num_datapoints').first(),
+                'most_studied_phenotype':  Observable2.objects.annotate(num_datapoints=Sum('phenotype__dataset__tested_num')).exclude(num_datapoints=None).order_by('-num_datapoints').first(),
+            }
+        )
     return render(request, 'yeastphenome/index.html', context)
 
 def about(request):
