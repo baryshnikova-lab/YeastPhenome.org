5d44f6c27cd716d77a4770f0e60a3904dad1f249
Anastasia Baryshnikova <abarysh@princeton.edu>
2016-04-29 18:12:28 -0400

    Simplified the search, the main Papers list view and added the filter to exclude not relevant papers

--- papers/views.py
+++ papers/views.py
@@ -1,192 +1,42 @@
-from django.db.models import Q,Count
+from django.db.models import Q
 from django.views import generic
-from django.views.generic.base import TemplateView
-from django.http.request import QueryDict
+from django.shortcuts import render
 
 from papers.models import Paper
-import operator
 import urllib2, re
-
-from phenotypes.models import Observable2
-from conditions.models import ConditionType
 
 # These only needed for zipo
 import os
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404
 from django.conf import settings
 from cStringIO import StringIO
 from zipfile import ZipFile
 
 
-class PaperIndexView(generic.ListView):
-    """A virtual class."""
-    model = Paper
-    template_name = 'papers/index.html'
-    context_object_name = 'papers_list'
+def paper_list_view(request):
 
-    the_filter = False  # this should be overloaded
-    GOT = False  # for caching multiple requests
-    template_ref = None  # for tweaking with the template
+    queryset = Paper.objects.all()
 
-    def get_queryset(self):
+    # Exclude the papers marked as "not relevant"
+    f = Q(data_statuses__status_name__exact='not relevant') | Q(tested_statuses__status_name__exact='not relevant')
+    queryset = queryset.exclude(f)
 
-        got = self.scrub_GET()
-        if 's' in got:
-            papers=[] # hold list of paper ids
+    if 'q' in request.GET:
+        q = request.GET['q']
+        f = Q(first_author__icontains=q) | Q(last_author__icontains=q) | Q(pmid__contains=q)
+        f = f | Q(dataset__phenotype__observable2__name__icontains=q)
+        f = f | Q(dataset__conditionset__conditions__type__name__icontains=q) | Q(dataset__conditionset__conditions__type__short_name__icontains=q)
+        queryset = queryset.filter(f)
+    else:
+        q = ''
 
-            for s in got.getlist('s'):
-                o2s=Observable2.objects.filter(name__icontains=s)
-                cts=ConditionType.objects.filter(
-                    Q(name__icontains=s)|Q(short_name__icontains=s))
+    queryset = queryset.distinct().order_by('pmid')
 
-                ps=set()
-                for o2 in o2s:
-                    for paper in o2.paper_list():
-                        ps.add(paper.id)
-                for ct in cts:
-                    for paper in ct.paper_list():
-                        ps.add(paper.id)
-
-                f=None
-                if s.isdigit():
-                    # we don't need to check PMID unless it's all digits
-                    f=(Q(first_author__icontains=s)|Q(last_author__icontains=s)|
-                       Q(pmid__contains=s))  # on icontains for PMID, it's a number
-                else:
-                    f=Q(first_author__icontains=s)|Q(last_author__icontains=s)
-
-                for paper in Paper.objects.exclude(pk__in=ps).filter(f):
-                    ps.add(paper.id)
-
-                papers.append(ps)
-
-            if 0 == len(papers):
-                return []
-            else:
-                # We want to return a ResultSet because we need to
-                # filter it again later.
-                return Paper.objects.filter(pk__in=set.intersection(*papers))
-
-        # if no query return everything
-        return super(PaperIndexView, self).get_queryset()
-
-
-    @classmethod
-    def scrub_GET_txt(self, get):
-        """Returns text suitable for putting in the search box."""
-        return self.got_txt(self._scrub_GET(get))
-
-    @classmethod
-    def got_txt(self,got):
-        """Same as scrub_GET_txt() but assume got is already scrubbed."""
-        return ' '.join(got.getlist('s'))
-
-    def scrub_GET(self):
-        """Returns a scrubbed self.request.GET, scrub it if we haven't done so
-        yet."""
-        if not(self.GOT):
-            self.GOT=self._scrub_GET(self.request.GET)
-        return self.GOT
-
-    @classmethod
-    def _scrub_GET(self,GET):
-        """Scrubbs a QueryDict object.  This function is subject to change
-        without notice."""
-        OUT=QueryDict(mutable=True)
-
-        # multilpe 's' get split by white space as well
-        if 's' in GET:
-            esses = GET.getlist('s')
-            s=[]
-            for ess in esses:
-                s.extend(ess.strip().split())
-            if 0 < len(s):
-                OUT.setlist('s',s)
-
-        # anything else is hacking.
-        return OUT
-
-    @classmethod
-    def context_update(cls,self_,qs,got):
-        out={}
-        key='num_%s' % (cls.template_ref)
-
-        if type(self_) == cls:
-            if cls.the_filter:
-                qs=qs.filter(cls.the_filter).distinct().\
-                    order_by('pub_date','pmid')
-                out['papers_list']=qs
-            out[key]=len(qs) # qs.count()
-            out['sub_navigation']=cls.template_ref.replace('_',' ').capitalize()
-        else:
-            if cls.the_filter:
-                out[key]=qs.filter(cls.the_filter).distinct().count()
-            else:
-                out[key]=len(qs)
-        return out
-
-    def get_context_data(self, **kwargs):
-        """Message the default context data to something we can use."""
-
-        context = super(PaperIndexView, self).get_context_data(**kwargs)
-        got=self.scrub_GET()
-
-        # filter out by pmid and author
-        papers=context['papers_list']
-        # Every thing is papers is in the results, now we need to
-        # check everything else.  Bleck.
-
-        context['s']=self.got_txt(got)
-        context['got'] = '?%s' % (got.urlencode())
-        if '?' == context['got']:
-            context['got'] = ''
-
-        # # Let get totals for each paper type
-        # context.update(PaperAllIndexView.context_update
-        #                (self,papers,got))
-        # context.update(PaperHaploidIndexView.context_update
-        #                (self,papers,got))
-        # context.update(PaperDiploidHomozygousIndexView.context_update
-        #                (self,papers,got))
-        # context.update(PaperDiploidHeterozygousIndexView.context_update
-        #                (self,papers,got))
-        # context.update(PaperQuantitativeIndexView.context_update
-        #                (self,papers,got))
-        # context.update(PaperDiscreteIndexView.context_update
-        #                (self,papers,got))
-
-        return context
-
-
-class PaperAllIndexView(PaperIndexView):
-    queryset = Paper.objects.order_by('pmid')
-    template_ref = 'all'
-
-
-class PaperHaploidIndexView(PaperIndexView):
-    the_filter = Q(dataset__collection__ploidy=1)
-    template_ref = 'haploid'
-
-
-class PaperDiploidHomozygousIndexView(PaperIndexView):
-    the_filter = Q(dataset__collection__shortname='hom')
-    template_ref = 'diploid_homozygous'
-
-
-class PaperDiploidHeterozygousIndexView(PaperIndexView):
-    the_filter = Q(dataset__collection__shortname='het')
-    template_ref = 'diploid_heterozygous'
-
-
-class PaperQuantitativeIndexView(PaperIndexView):
-    the_filter = Q(dataset__data_available='quantitative')
-    template_ref = 'quantitative'
-
-
-class PaperDiscreteIndexView(PaperIndexView):
-    the_filter = Q(dataset__data_available='discrete')
-    template_ref='discrete'
+    return render(request, 'papers/index.html', {
+        'papers_list': queryset,
+        'q': q,
+    })
 
 
 class PaperDetailView(generic.DetailView):
@@ -217,8 +67,8 @@ class PaperDetailView(generic.DetailView):
         # Credit now given
 
         # Fetch PMID info if we have to
-        pmid=obj.pmid
-        if 0==pmid:
+        pmid = obj.pmid
+        if pmid == 0:
             # If we have no PMID just bail
             return context
         ml = os.path.join(settings.MEDLINE_DIR, "%s.txt" % (pmid))
@@ -226,7 +76,7 @@ class PaperDetailView(generic.DetailView):
             pass
         else:
             try:
-                out = open(ml,'w')
+                out = open(ml, 'w')
                 url = "https://www.ncbi.nlm.nih.gov/pubmed/%s?report=medline&format=text" % (pmid)
                 r = urllib2.urlopen(url)
                 out.write(re.sub('<[^<]+?>', '', r.read()).strip())
--- yeastphenome/views.py
+++ yeastphenome/views.py
@@ -1,43 +1,27 @@
-from django.shortcuts import get_object_or_404, render
-from django.db.models import Count, Sum
+from django.shortcuts import render
 
-from papers.models import Paper, Data
-from conditions.models import Condition, ConditionSet
+from yeastphenome.forms import SearchForm
+
+from papers.models import Paper
+from conditions.models import Condition
 from phenotypes.models import Observable2
-from papers.views import PaperIndexView
-
-import datetime
 
 
 def index(request):
     latest_paper = Paper.objects.all().latest().modified_on
-    latest_condition = Condition.objects.all().latest().modified_on
 
-    if latest_condition is None:
-        # fudging NULL entries in the database
-        latest_condition = datetime.date(1,1,1)
+    form = SearchForm()
 
     context = {
         'paper_num_total': Paper.objects.all().count(),
         'phenotype_num_total': Observable2.objects.exclude(phenotype__dataset = None).count(),
         'condition_num': Condition.objects.all().count(),
-        'updated': max(latest_paper, latest_condition),
-
-        # To repopulate the search box
-        's':PaperIndexView.scrub_GET_txt(request.GET),
+        'updated': latest_paper,
+        'form': form,
     }
 
-    if 'verbose' in request.GET:
-        context.update(
-            {
-                'paper_num_with_data': Data.objects.values('dataset__paper').distinct().count(),
-                'phenotype_num_with_data': Data.objects.values('dataset__phenotype__observable2__name').distinct().count(),
-                'most_studied_conditionset': ConditionSet.objects.exclude(name='standard').exclude(name='').values('name').annotate(num_datapoints=Sum('dataset__tested_num')).order_by('-num_datapoints').first(),
-                'most_studied_phenotype':  Observable2.objects.annotate(num_datapoints=Sum('phenotype__dataset__tested_num')).exclude(num_datapoints=None).order_by('-num_datapoints').first(),
-            }
-        )
     return render(request, 'yeastphenome/index.html', context)
 
+
 def about(request):
-    """Really just to have access to the dynamic template."""
     return render(request, 'yeastphenome/about.html')
