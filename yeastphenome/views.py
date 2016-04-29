50100118477782d3e9b49e88bea4b3a9a4d47377
Anastasia Baryshnikova <abarysh@princeton.edu>
2016-04-29 18:21:19 -0400

    Small fix to reflect the correct count of papers on main page

--- yeastphenome/views.py
+++ yeastphenome/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 
 from yeastphenome.forms import SearchForm
 
@@ -8,12 +9,18 @@ from phenotypes.models import Observable2
 
 
 def index(request):
-    latest_paper = Paper.objects.all().latest().modified_on
+
+    queryset = Paper.objects.all()
+    latest_paper = queryset.latest().modified_on
 
     form = SearchForm()
 
+    # Exclude the papers marked as "not relevant"
+    f = Q(data_statuses__status_name__exact='not relevant') | Q(tested_statuses__status_name__exact='not relevant')
+    queryset = queryset.exclude(f)
+
     context = {
-        'paper_num_total': Paper.objects.all().count(),
+        'paper_num_total': queryset.count(),
         'phenotype_num_total': Observable2.objects.exclude(phenotype__dataset = None).count(),
         'condition_num': Condition.objects.all().count(),
         'updated': latest_paper,
