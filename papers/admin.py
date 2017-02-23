4b95029ca8205460da3a3b6dec4ef9d181de30ca
Anastasia Baryshnikova <abarysh@princeton.edu>
2017-02-22 18:16:10 -0800

    Debug

--- papers/admin.py
+++ papers/admin.py
@@ -62,19 +62,19 @@ class DatasetInline(ImprovedTabularInline):
     admin_change_link.allow_tags = True
 
     def make_a_copy_link(self, obj):
-        query_string = ''
-        query_dict = {'_popup': 1}
-        flds = obj._meta.get_fields()
-        for f in flds:
-            f_name = f.name
-            if isinstance(f, ForeignKey):
-                f_name += "_id"
-            f_value = str(getattr(obj, f_name))
-            if f.name != 'id' and f_value != 'None':
-                query_string += "&" + f.name + "=" + f_value
-                query_dict[f.name] = f_value
-        query_string = urlencode(query_dict)
-        #return '<a id="id_user" href="%s?%s" onclick="return showAddAnotherPopup(this);">Make a copy</a>' % (reverse("admin:datasets_dataset_add"), query_string)
+        query_string = 'test'
+        # query_dict = {'_popup': 1}
+        # flds = obj._meta.get_fields()
+        # for f in flds:
+        #     f_name = f.name
+        #     if isinstance(f, ForeignKey):
+        #         f_name += "_id"
+        #     f_value = str(getattr(obj, f_name))
+        #     if f.name != 'id' and f_value != 'None':
+        #         query_string += "&" + f.name + "=" + f_value
+        #         query_dict[f.name] = f_value
+        # query_string = urlencode(query_dict)
+        # return '<a id="id_user" href="%s?%s" onclick="return showAddAnotherPopup(this);">Make a copy</a>' % (reverse("admin:datasets_dataset_add"), query_string)
         return query_string
     make_a_copy_link.allow_tags = True
 
