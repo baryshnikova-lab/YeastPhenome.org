155b015dd6b83a11d553899f010a41509a4a5d9e
Anastasia Baryshnikova <abarysh@princeton.edu>
2017-02-22 18:55:29 -0800

    debug

--- papers/admin.py
+++ papers/admin.py
@@ -74,7 +74,7 @@ class DatasetInline(ImprovedTabularInline):
             f_name = f.name
             if isinstance(f, ForeignKey):
                 f_name += "_id"
-            f_value = str(getattr(obj, f_name))
+            f_value = getattr(obj, f_name)
             query_string += f_value
             # if f.name != 'id' and f_value != 'None':
             #     query_string += "&" + f.name + "=" + f_value
