09719854eee3551e94b35884d636fb1a00019623
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2017-04-01 16:01:11 -0700

    Updated zip file download

--- papers/urls.py
+++ papers/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)/$', views.PaperDetailView.as_view(), name='detail'),
 
     # Data
-    url(r'^(?P<pk>\d+).zip$', views.download_zip, name='download_zip'),
+    url(r'^(?P<paper_id>\d+)/YeastPhenome_(?P<paper_pmid>\d+).zip$', views.download_zip, name='download_zip'),
     url(r'^(?P<paper_id>\d+)/YeastPhenome_(\d+)_datasets_list.txt$', views.paper_datasets, name='paper_datasets'),
 ]
