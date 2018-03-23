8d6ef237bf9da83f66b653cbb12a5b7cc8cafc87
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2018-03-22 17:28:30 -0700

    Added Current stats

--- yeastphenome/views.py
+++ yeastphenome/views.py
@@ -5,7 +5,8 @@ from yeastphenome.forms import SearchForm
 
 from papers.models import Paper
 from conditions.models import ConditionSet
-from phenotypes.models import Observable2
+from phenotypes.models import Observable2, Phenotype
+from datasets.models import Dataset
 
 
 def index(request):
@@ -32,4 +33,37 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'yeastphenome/about.html')
+
+    papers_queryset = Paper.objects.all()
+    phenotypes_queryset = Phenotype.objects.all()
+    conditions_queryset = ConditionSet.objects.all()
+    datasets_queryset = Dataset.objects.all()
+
+    # Total number of papers to process
+    f = Q(latest_data_status__status__status_name__exact='not relevant') | \
+        Q(latest_tested_status__status__status_name__exact='not relevant')
+    papers_nr = papers_queryset.exclude(f).count()
+
+    # Number of papers processed and loaded
+    f = Q(latest_data_status__status__status_name__exact='loaded') & \
+        Q(latest_tested_status__status__status_name__in=['loaded', 'abandoned', 'not available'])
+    papers_queryset = papers_queryset.filter(f)
+    papers_processed_nr = papers_queryset.count()
+
+    # Number of phenotypes
+    f = Q(dataset__paper__in=papers_queryset)
+    phenotypes_nr = phenotypes_queryset.filter(f).distinct().count()
+
+    # Number of conditions
+    f = Q(dataset__paper__in=papers_queryset)
+    conditions_nr = conditions_queryset.filter(f).distinct().count()
+
+    # Number of datasets
+    f = Q(paper__in=papers_queryset)
+    datasets_nr = datasets_queryset.filter(f).distinct().count()
+
+    context = {'papers_nr': papers_nr, 'papers_processed_nr': papers_processed_nr,
+               'phenotypes_nr': phenotypes_nr, 'conditions_nr': conditions_nr,
+               'datasets_nr': datasets_nr}
+
+    return render(request, 'yeastphenome/about.html', context)
