e12d7f680fd7aadd0fcb62da97cc37f6c2d60f7f
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2018-04-11 19:53:39 -0700

    Unified all summary stats for consistency

--- yeastphenome/views.py
+++ yeastphenome/views.py
@@ -9,30 +9,7 @@ from phenotypes.models import Observable2, Phenotype
 from datasets.models import Dataset
 
 
-def index(request):
-
-    queryset = Paper.objects.all()
-    latest_paper = queryset.latest().modified_on
-
-    form = SearchForm()
-
-    # Exclude the papers marked as "not relevant"
-    f = Q(latest_data_status__status__status_name__exact='not relevant') | \
-        Q(latest_tested_status__status__status_name__exact='not relevant')
-    queryset = queryset.exclude(f)
-
-    context = {
-        'paper_num_total': queryset.count(),
-        'phenotype_num_total': Observable2.objects.exclude(phenotype__dataset__isnull=True).count(),
-        'condition_num': ConditionSet.objects.all().count(),
-        'updated': latest_paper,
-        'form': form,
-    }
-
-    return render(request, 'yeastphenome/index.html', context)
-
-
-def about(request):
+def get_latest_stats():
 
     papers_queryset = Paper.objects.all()
     phenotypes_queryset = Phenotype.objects.all()
@@ -40,9 +17,15 @@ def about(request):
     datasets_queryset = Dataset.objects.all()
 
     # Total number of papers to process
-    f = Q(latest_data_status__status__status_name__exact='not relevant') | \
-        Q(latest_tested_status__status__status_name__exact='not relevant')
+    f = Q(latest_data_status__status__status_name__in=['not relevant', 'abandoned', 'not available'])
     papers_nr = papers_queryset.exclude(f).count()
+
+    # Latest modified paper
+    updated_on = papers_queryset.latest().modified_on
+
+    # Number of hopeless papers
+    f = Q(latest_data_status__status__status_name__in=['abandoned', 'not available'])
+    papers_hopeless_nr = papers_queryset.filter(f).count()
 
     # Number of papers processed and loaded
     f = Q(latest_data_status__status__status_name__exact='loaded') & \
@@ -62,8 +45,27 @@ def about(request):
     f = Q(paper__in=papers_queryset)
     datasets_nr = datasets_queryset.filter(f).distinct().count()
 
-    context = {'papers_nr': papers_nr, 'papers_processed_nr': papers_processed_nr,
+    context = {'papers_nr': papers_nr, 'papers_hopeless_nr': papers_hopeless_nr,
+               'papers_processed_nr': papers_processed_nr,
                'phenotypes_nr': phenotypes_nr, 'conditions_nr': conditions_nr,
-               'datasets_nr': datasets_nr}
+               'datasets_nr': datasets_nr,
+               'updated_on': updated_on}
+
+    return context
+
+
+def index(request):
+
+    form = SearchForm()
+
+    context = get_latest_stats()
+    context['form'] = form
+
+    return render(request, 'yeastphenome/index.html', context)
+
+
+def about(request):
+
+    context = get_latest_stats()
 
     return render(request, 'yeastphenome/about.html', context)
