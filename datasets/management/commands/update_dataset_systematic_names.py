22ba014a58f3998250de8bb0ae55b71798f7dd49
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2019-02-09 19:54:03 -0800

    Testing

--- datasets/management/commands/update_dataset_systematic_names.py
+++ datasets/management/commands/update_dataset_systematic_names.py
@@ -8,6 +8,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
+        # super(Command, self).handle(*args, **options)
+
         all_datasets = Dataset.objects.all()
 
         for dataset in tqdm(all_datasets):
