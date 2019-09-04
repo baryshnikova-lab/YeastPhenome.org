4866524bb3bfe5630d86d057303867989b90385f
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2019-09-03 18:20:30 -0700

    Added 2 management commands in preparation for data release

---
+++ datasets/management/commands/check_release_permissions.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from django.db import IntegrityError
+
+from datasets.models import Dataset
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        # super(Command, self).handle(*args, **options)cd
+
+        all_datasets = Dataset.objects.exclude(data_source__release=True).\
+            filter(paper__latest_data_status__status__status_name__in=['loaded']).distinct()
+
+        for dataset in all_datasets:
+            self.stdout.write('%s' % dataset.name)
+
+        self.stdout.write('Finished.')
