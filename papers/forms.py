87d13b957285f2c3164dae460bcca5a5d323aaac
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2020-02-28 17:04:30 -0800

    Modified notes and private_notes appearance in admin for consistency

--- papers/forms.py
+++ papers/forms.py
@@ -8,5 +8,7 @@ class PaperModelForm(ModelForm):
         model = Paper
         fields = ('__all__')
         widgets = {
+            'notes': Textarea(attrs={'cols': 85}),
+            'private_notes': Textarea(attrs={'cols': 85}),
             'data_abstract': Textarea(attrs={'cols': 85, 'rows': 3}),
         }
