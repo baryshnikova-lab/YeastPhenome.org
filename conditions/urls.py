88721b1335329c5bfda7e933a5befb2afea84996
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2020-05-21 10:41:25 -0700

    Rename view

--- conditions/urls.py
+++ conditions/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<pk>\d+)/$', views.ConditionDetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>\d+)/$', views.ConditiontypeDetailView.as_view(), name='detail'),
     url(r'^chebi/(?P<class_id>\d+)/$', views.conditionclass, name='class'),
     url(r'^media/(?P<pk>\d+)/$', views.MediumDetailView.as_view(), name='medium_detail'),
     url(r'^conditionset/(?P<pk>\d+)/$', views.ConditionSetDetailView.as_view(), name='conditionset_detail'),
