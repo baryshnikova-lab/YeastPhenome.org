70e44fa8273813b6c6f24c67a9f61f8fb3ad480d
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2020-05-21 10:35:28 -0700

    MAJOR: added Observable (flat) to replace Obvservable2 (hierarchy)

--- phenotypes/admin.py
+++ phenotypes/admin.py
@@ -2,7 +2,49 @@ from django.contrib import admin
 from mptt.admin import MPTTModelAdmin
 from django.http import HttpResponse
 
-from phenotypes.models import MutantType, Observable2, Phenotype, Measurement
+from phenotypes.models import MutantType, Observable, Observable2, Phenotype, Measurement, Tag
+from common.admin_util import ImprovedModelAdmin, ImprovedTabularInline
+
+
+class ObservableAdmin(ImprovedModelAdmin):
+    list_per_page = 50
+    list_display = ['name', 'description', 'tags_str_list']
+    search_fields = ['name', 'description', 'tags__name']
+    fields = ('name', 'description', 'tags', 'phenotypes_edit_link_list', 'datasets_edit_link_list', )
+    readonly_fields = ('phenotypes_edit_link_list', 'datasets_edit_link_list', )
+    raw_id_fields = ('tags',)
+    ordering = ['name']
+
+    def response_change(self, request, obj):
+        if request.GET.get('_popup') == '1':
+            return HttpResponse(
+                '<script type="text/javascript">window.opener.location.reload(); window.close();</script>')
+        return super(ObservableAdmin, self).response_change(request, obj)
+
+    def response_add(self, request, obj, post_url_continue=None):
+        if request.GET.get('_popup') == '1':
+            return HttpResponse('<script type="text/javascript">window.opener.location.reload(); window.close();</script>')
+        return super(ObservableAdmin, self).response_add(request, obj, post_url_continue)
+
+
+class TagAdmin(ImprovedModelAdmin):
+    list_per_page = 50
+    list_display = ['name', 'description', 'observables_str_list']
+    search_fields = ['name', 'description']
+    fields = ('name', 'description', 'observables_edit_link_list')
+    readonly_fields = ('observables_edit_link_list', )
+    ordering = ['name']
+
+    def response_change(self, request, obj):
+        if request.GET.get('_popup') == '1':
+            return HttpResponse(
+                '<script type="text/javascript">window.opener.location.reload(); window.close();</script>')
+        return super(TagAdmin, self).response_change(request, obj)
+
+    def response_add(self, request, obj, post_url_continue=None):
+        if request.GET.get('_popup') == '1':
+            return HttpResponse('<script type="text/javascript">window.opener.location.reload(); window.close();</script>')
+        return super(TagAdmin, self).response_add(request, obj, post_url_continue)
 
 
 class Observable2Admin(MPTTModelAdmin):
@@ -40,11 +82,12 @@ class MutantTypeAdmin(admin.ModelAdmin):
 
 class PhenotypeAdmin(admin.ModelAdmin):
     list_per_page = 1000
-    list_display = ['observable2_name', 'name', 'reporter', 'papers_edit_link_list']
-    ordering = ['observable2__ancestry']
+    list_display = ['name', 'reporter', 'papers_edit_link_list']
+    # ordering = ['observable2__ancestry']
     search_fields = ['name', ]
-    fields = ('name', 'description', 'observable2', 'reporter', 'measurement', )
-    raw_id_fields = ('measurement', )
+    fields = ('name', 'description', 'observable2', 'observable', 'reporter', 'measurement', 'datasets_edit_link_list', )
+    raw_id_fields = ('measurement', 'observable', )
+    readonly_fields = ('datasets_edit_link_list', )
 
     def response_change(self, request, obj):
         if request.GET.get('_popup') == '1':
@@ -63,4 +106,6 @@ class MeasurementAdmin(admin.ModelAdmin):
 admin.site.register(Phenotype, PhenotypeAdmin)
 admin.site.register(MutantType, MutantTypeAdmin)
 admin.site.register(Observable2, Observable2Admin)
+admin.site.register(Observable, ObservableAdmin)
+admin.site.register(Tag, TagAdmin)
 admin.site.register(Measurement, MeasurementAdmin)
--- phenotypes/models.py
+++ phenotypes/models.py
@@ -5,12 +5,92 @@ from mptt.models import MPTTModel, TreeForeignKey
 from django.db.models import Q
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+    description = models.TextField(max_length=1000, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def link_detail(self):
+        return '<a href="%s">%s</a>' % (reverse("phenotypes:tag", args=(self.id,)), self)
+    link_detail.allow_tags = True
+
+    def link_edit(self):
+        html = '<a href="%s">%s</a>' % (reverse("admin:phenotypes_tag_change", args=(self.id,)), self)
+        return html
+    link_edit.allow_tags = True
+
+    def observables(self):
+        return apps.get_model('phenotypes', 'Observable').objects.filter(tags=self).order_by('name').all()
+
+    def observables_str_list(self):
+        return '; '.join([str(o) for o in self.observables()])
+
+    def observables_edit_link_list(self):
+        html = '<ul>'
+        html = html + '<li>'.join([p.link_edit() for p in self.observables()])
+        html = html + '</ul>'
+        return html
+    observables_edit_link_list.allow_tags = True
+
+
+class Observable(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    modified_on = models.DateField(auto_now=True, null=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    class Meta:
+        get_latest_by = 'modified_on'
+
+    def __str__(self):
+        return u'%s' % self.name
+
+    def link_edit(self):
+        html = '<a href="%s">%s</a>' % (reverse("admin:phenotypes_observable_change", args=(self.id,)), self)
+        return html
+    link_edit.allow_tags = True
+
+    def tags_str_list(self):
+        return ", ".join([(u'%s' % t) for t in self.tags.all()])
+
+    def tags_edit_link_list(self):
+        html = '<ul>'
+        html = html + '<li>'.join([p.link_edit() for p in self.tags.all()])
+        html = html + '</ul>'
+        return html
+    tags_edit_link_list.allow_tags = True
+
+    def phenotypes(self):
+        return apps.get_model('phenotypes', 'Phenotype').objects.filter(observable=self).distinct()
+
+    def phenotypes_list(self):
+        return '; '.join([str(p) for p in self.phenotypes()[:20]])
+
+    def phenotypes_edit_link_list(self):
+        html = '<ul>'
+        html = html + '<li>'.join([p.link_edit() for p in self.phenotypes()[:20]])
+        html = html + '</ul>'
+        return html
+    phenotypes_edit_link_list.allow_tags = True
+
+    def datasets(self):
+        return apps.get_model('datasets', 'Dataset').objects.filter(phenotype__observable=self).all()
+
+    def datasets_edit_link_list(self):
+        html = '<ul>'
+        html = html + '<li>'.join([d.link_edit() for d in self.datasets()[:50]])
+        html = html + '</ul>'
+        return html
+    datasets_edit_link_list.allow_tags = True
+
+
 class Observable2(MPTTModel):
     """Generally the way to fetch phenotypes."""
 
     name = models.CharField(max_length=200)
     parent = TreeForeignKey('self', null=True, blank=True, related_name='children')
-    #,to_field='ancestry') # See ./admin.py:Observable2Admin.to_field_allowed
     description = models.TextField(blank=True, null=True)
     definition = models.TextField(blank=True, null=True)
     modified_on = models.DateField(auto_now=True, null=True)
@@ -99,7 +179,7 @@ class Observable2(MPTTModel):
     conditiontypes_link_list.allow_tags = True
 
     def datasets(self):
-        return apps.get_model('datasets', 'Dataset').objects\
+        return apps.get_model('papers', 'Dataset').objects\
             .filter(phenotype__observable2=self) \
             .exclude(paper__latest_data_status__status__name='not relevant')\
             .distinct()
@@ -131,6 +211,7 @@ class Phenotype(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     observable2 = TreeForeignKey(Observable2)
+    observable = models.ForeignKey(Observable, blank=False, null=False)
     reporter = models.CharField(max_length=200, blank=True, null=True)
     measurement = models.ForeignKey(Measurement, blank=True, null=True)
     modified_on = models.DateField(auto_now=True, null=True)
@@ -172,6 +253,16 @@ class Phenotype(models.Model):
     def papers_edit_link_list(self):
         return ', '.join([p.link_edit() for p in self.papers()])
     papers_edit_link_list.allow_tags = True
+
+    def datasets(self):
+        return apps.get_model('datasets', 'Dataset').objects.filter(phenotype=self).all()
+
+    def datasets_edit_link_list(self):
+        html = '<ul>'
+        html = html + '<li>'.join([d.link_edit() for d in self.datasets()[:50]])
+        html = html + '</ul>'
+        return html
+    datasets_edit_link_list.allow_tags = True
 
 
 class MutantType(models.Model):
